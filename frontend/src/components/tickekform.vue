6669330b3979847d42f6ce45f1c4bd84ad3034df
mm04 <muhqi407@gmail.com>
2025-05-10 07:53:11 +0700

    popup ส่ง ticket

--- frontend/src/components/tickekform.vue
+++ frontend/src/components/tickekform.vue
@@ -3,27 +3,27 @@
     <card>
         <cardcontent>
             <form @submit.prevent="submitTicket" class="space-y-4">
-            <input v-model="title" type="text" placeholder="หัวข้อปัญหา" class="input" required />
-            <textarea v-model="description" placeholder="รายละเอียด" class="input" rows="5" />
+                <input v-model="title" type="text" placeholder="หัวข้อปัญหา" class="input" required />
+                <textarea v-model="description" placeholder="รายละเอียด" class="input" rows="5" />
 
-            <input v-model="contact" type="text" placeholder="ข้อมูลติดต่อ" class="input" />
-            <input v-model="department" type="text" placeholder="แผนก" class="input" />
+                <input v-model="contact" type="text" placeholder="ข้อมูลติดต่อ" class="input" />
+                <input v-model="department" type="text" placeholder="แผนก" class="input" />
 
-            <select v-model="type_id" class="input">
-                <option disabled value="">-- เลือกประเภท --</option>
-                <option v-for="type in types" :key="type.id" :value="type.id">
-                    {{ `${type.name}: `}} {{ type.description }}
-                </option>
-            </select>
+                <select v-model="type_id" class="input">
+                    <option disabled value="">-- เลือกประเภท --</option>
+                    <option v-for="type in types" :key="type.id" :value="type.id">
+                        {{ `${type.name}: ` }} {{ type.description }}
+                    </option>
+                </select>
 
-            <select v-model="priority" class="input">
-                <option disabled value="">-- เลือกระดับความสำคัญ --</option>
-                <option value="low">ต่ำ</option>
-                <option value="medium">กลาง</option>
-                <option value="high">สูง</option>
-            </select>
+                <select v-model="priority" class="input">
+                    <option disabled value="">-- เลือกระดับความสำคัญ --</option>
+                    <option value="low">ต่ำ</option>
+                    <option value="medium">กลาง</option>
+                    <option value="high">สูง</option>
+                </select>
 
-            <!-- <select v-model="status" class="input">
+                <!-- <select v-model="status" class="input">
                 <option disabled value="">-- สถานะ --</option>
                 <option value="open">เปิด</option>
                 <option value="in_progress">กำลังดำเนินการ</option>
@@ -31,27 +31,25 @@
                 <option value="closed">ปิดแล้ว</option>
             </select> -->
 
-            <input type="file" multiple 
-                @change="handleFileChange" 
-                class="input" accept=".pdf, .jpg, .jpeg, .png"/>
-            <!-- แสดงรายการไฟล์ที่เลือก -->
-            <ul class="mt-2 space-y-1">
-                <li v-for="(file, index) in files" :key="index"
-                    class="flex justify-between items-center bg-gray-100 px-3 py-1 rounded">
-                    <span class="truncate max-w-xs">{{ file.name }}</span>
-                    <button type="button" @click="removeFile(index)" class="text-red-500 hover:underline text-sm">
-                        ลบ
-                    </button>
-                </li>
-            </ul>
+                <input type="file" multiple @change="handleFileChange" class="input" accept=".pdf, .jpg, .jpeg, .png" />
+                <!-- แสดงรายการไฟล์ที่เลือก -->
+                <ul class="mt-2 space-y-1">
+                    <li v-for="(file, index) in files" :key="index"
+                        class="flex justify-between items-center bg-gray-100 px-3 py-1 rounded">
+                        <span class="truncate max-w-xs">{{ file.name }}</span>
+                        <button type="button" @click="removeFile(index)" class="text-red-500 hover:underline text-sm">
+                            ลบ
+                        </button>
+                    </li>
+                </ul>
 
-            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-                ส่ง Ticket
-            </button>
+                <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+                    ส่ง Ticket
+                </button>
             </form>
         </cardcontent>
     </card>
-    
+
 </template>
 
 <script setup lang="ts">
@@ -60,6 +58,10 @@ import axios from 'axios'
 import { defineStore } from 'pinia'
 import { jwtDecode } from 'jwt-decode'
 import { config } from '@/config';
+import Swal from 'sweetalert2'
+import api from '@/api/axios-instance'
+
+
 
 import cardtitle from '@/ui/cardtitle.vue';
 import card from '@/ui/card.vue';
@@ -72,37 +74,37 @@ const priority = ref('')
 // const status = ref('')
 const contact = ref('')
 const department = ref('')
-const files = ref < File[] > ([])
+const files = ref<File[]>([])
 const types = ref([])
 
 function handleFileChange(event: Event) {
-const input = event.target as HTMLInputElement
-if (!input.files) return
+    const input = event.target as HTMLInputElement
+    if (!input.files) return
 
-const selectedFiles = Array.from(input.files)
-const allowedTypes = ['application/pdf', 'image/jpeg', 'image/png']
+    const selectedFiles = Array.from(input.files)
+    const allowedTypes = ['application/pdf', 'image/jpeg', 'image/png']
 
-if (selectedFiles.length > 5) {
-    alert('ไม่สามารถแนบไฟล์เกิน 5 ไฟล์ได้')
-    input.value = '' // รีเซ็ต input
-    return
-}
-
-const validFiles: File[] = []
-for (let i = 0; i < selectedFiles.length; i++) {
-    const file = selectedFiles[i]
-    if (allowedTypes.includes(file.type)) {
-        validFiles.push(file)
-    } else {
-        alert(`ไม่อนุญาตให้แนบไฟล์ประเภท: ${file.type}`)
+    if (selectedFiles.length > 5) {
+        alert('ไม่สามารถแนบไฟล์เกิน 5 ไฟล์ได้')
+        input.value = '' // รีเซ็ต input
+        return
     }
-}
 
-files.value = validFiles
+    const validFiles: File[] = []
+    for (let i = 0; i < selectedFiles.length; i++) {
+        const file = selectedFiles[i]
+        if (allowedTypes.includes(file.type)) {
+            validFiles.push(file)
+        } else {
+            alert(`ไม่อนุญาตให้แนบไฟล์ประเภท: ${file.type}`)
+        }
+    }
+
+    files.value = validFiles
 }
 
 onMounted(async () => {
-    const res = await axios.get(`${config.apiUrl}/api/types`)
+    const res = await api.get(`/types`)
     types.value = res.data.data
     // console.log('type: ', types.value)
 })
@@ -110,45 +112,99 @@ onMounted(async () => {
 const submitTicket = async () => {
     const token = localStorage.getItem('accessToken')
     if (!token) return alert('กรุณาเข้าสู่ระบบ')
-
+    
     const decoded: any = jwtDecode(token)
     const userId = decoded.userId || decoded.id
 
     if (!title.value || !description.value || !type_id.value || !priority.value || !contact.value || !department.value) {
-    alert('กรุณากรอกข้อมูลให้ครบทุกช่อง');
-    return;
+        alert('กรุณากรอกข้อมูลให้ครบทุกช่อง');
+        return;
     }
 
-    const formData = new FormData()
-    formData.append('title', title.value)
-    formData.append('description', description.value)
-    formData.append('type_id', type_id.value)
-    formData.append('priority', priority.value)
-    // formData.append('status', status.value)
-    formData.append('contact', contact.value)
-    formData.append('department', department.value)
-    formData.append('user_id', userId)
-
-    files.value.forEach(file => {
-        formData.append('files', file)
+    const result = await Swal.fire({
+        title: 'ส่ง Ticket?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'ส่ง',
+        cancelButtonText: 'ยกเลิก',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
     })
+        if (result.isConfirmed) {
+            const formData = new FormData()
+            formData.append('title', title.value)
+            formData.append('description', description.value)
+            formData.append('type_id', type_id.value)
+            formData.append('priority', priority.value)
+            // formData.append('status', status.value)
+            formData.append('contact', contact.value)
+            formData.append('department', department.value)
+            formData.append('user_id', userId)
 
-    try {
-        await axios.post(`${config.apiUrl}/api/tickets/create`, formData, {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'multipart/form-data'
+            files.value.forEach(file => {
+                formData.append('files', file)
+            })
+
+            try {
+                await api.post(`/tickets/create`, formData, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                Swal.fire({
+                    title: 'ส่งแล้ว!',
+                    text: 'ส่งข้อมูลสำเร็จ',
+                    icon: 'success',
+                    showClass: {
+                        popup: 'animate__animated animate__heartBeat'
+                    },
+                    timer: 2000
+                });
+            } catch (err) {
+                await Swal.fire(
+                    'ผิดพลาด!',
+                    'ไม่สามารถlส่งข้อมูลได้: ' + err.message,
+                    'error'
+                )
             }
-        })
-        alert('ส่ง Ticket สำเร็จ')
-    } catch (err) {
-        console.error(err)
-        alert('เกิดข้อผิดพลาด')
+        }
+}
+    //     const formData = new FormData()
+    //     formData.append('title', title.value)
+    //     formData.append('description', description.value)
+    //     formData.append('type_id', type_id.value)
+    //     formData.append('priority', priority.value)
+    //     // formData.append('status', status.value)
+    //     formData.append('contact', contact.value)
+    //     formData.append('department', department.value)
+    //     formData.append('user_id', userId)
+
+    //     files.value.forEach(file => {
+    //         formData.append('files', file)
+    //     })
+
+    //     try {
+    //         await axios.post(`${config.apiUrl}/api/tickets/create`, formData, {
+    //             headers: {
+    //                 'Authorization': `Bearer ${token}`,
+    //                 'Content-Type': 'multipart/form-data'
+    //             }
+    //         })
+    //         alert('ส่ง Ticket สำเร็จ')
+    //     } catch (err) {
+    //         console.error(err)
+    //         alert('เกิดข้อผิดพลาด')
+    //     }
+    // }
+    const removeFile = (index: number) => {
+        files.value.splice(index, 1)
     }
-}
-const removeFile = (index: number) => {
-  files.value.splice(index, 1)
-}
+
 </script>
 
 <style scoped>
