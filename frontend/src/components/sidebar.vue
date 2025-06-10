53fe65b401af3b3eb7fb8d08e7ccb4bbc2db0482
mm04 <muhqi407@gmail.com>
2025-06-10 10:51:56 +0700

    Update sidebar navigation buttons to center text

--- frontend/src/components/sidebar.vue
+++ frontend/src/components/sidebar.vue
@@ -18,27 +18,27 @@
             </div>
         </router-link>
         <nav class="space-y-4 p-4" v-if="isAdmin">
-            <button @click="navigateTo('/dashboard')" class="w-full text-left hover:bg-gray-800 p-2 rounded">Dashboard</button>
-            <button @click="navigateTo('/users')" class="w-full text-left hover:bg-gray-800 p-2 rounded">ผู้ใช้งาน</button>
-            <button @click="navigateTo('/types')" class="w-full text-left hover:bg-gray-800 p-2 rounded">จัดการหมวดหมู่</button>
-            <button @click="navigateTo('/departments')" class="w-full text-left hover:bg-gray-800 p-2 rounded">จัดการแผนก</button>
-            <button @click="navigateTo('/profile')" class="w-full text-left hover:bg-gray-800 p-2 rounded">Profile</button>
+            <button @click="navigateTo('/dashboard')" class="w-full text-center hover:bg-gray-800 p-2 rounded">Dashboard</button>
+            <button @click="navigateTo('/users')" class="w-full text-center hover:bg-gray-800 p-2 rounded">ผู้ใช้งาน</button>
+            <button @click="navigateTo('/types')" class="w-full text-center hover:bg-gray-800 p-2 rounded">จัดการหมวดหมู่</button>
+            <button @click="navigateTo('/departments')" class="w-full text-center hover:bg-gray-800 p-2 rounded">จัดการแผนก</button>
+            <button @click="navigateTo('/profile')" class="w-full text-center hover:bg-gray-800 p-2 rounded">Profile</button>
         </nav>
 
         <nav class="space-y-4 p-4" v-else-if="isOfficer">
-            <button @click="navigateTo('/home')" class="w-full text-left hover:bg-gray-800 p-2 rounded">New Ticket</button>
-            <button @click="navigateTo('/MyTickets')" class="w-full text-left hover:bg-gray-800 p-2 rounded">My Ticket</button>
-            <button @click="navigateTo('/dashboard')" class="w-full text-left hover:bg-gray-800 p-2 rounded">Dashboard</button>
-            <button @click="navigateTo('/users')" class="w-full text-left hover:bg-gray-800 p-2 rounded">ผู้ใช้งาน</button>
-            <button @click="navigateTo('/types')" class="w-full text-left hover:bg-gray-800 p-2 rounded">จัดการหมวดหมู่</button>
-            <button @click="navigateTo('/departments')" class="w-full text-left hover:bg-gray-800 p-2 rounded">จัดการแผนก</button>
-            <button @click="navigateTo('/profile')" class="w-full text-left hover:bg-gray-800 p-2 rounded">Profile</button>
+            <button @click="navigateTo('/home')" class="w-full text-center hover:bg-gray-800 p-2 rounded">New Ticket</button>
+            <button @click="navigateTo('/MyTickets')" class="w-full text-center hover:bg-gray-800 p-2 rounded">My Ticket</button>
+            <button @click="navigateTo('/dashboard')" class="w-full text-center hover:bg-gray-800 p-2 rounded">Dashboard</button>
+            <button @click="navigateTo('/users')" class="w-full text-center hover:bg-gray-800 p-2 rounded">ผู้ใช้งาน</button>
+            <button @click="navigateTo('/types')" class="w-full text-center hover:bg-gray-800 p-2 rounded">จัดการหมวดหมู่</button>
+            <button @click="navigateTo('/departments')" class="w-full text-center hover:bg-gray-800 p-2 rounded">จัดการแผนก</button>
+            <button @click="navigateTo('/profile')" class="w-full text-center hover:bg-gray-800 p-2 rounded">Profile</button>
         </nav>
 
         <nav class="space-y-4 p-4" v-else-if="isUser">
-            <button @click="navigateTo('/home')" class="w-full text-left hover:bg-gray-800 p-2 rounded">New Ticket</button>
-            <button @click="navigateTo('/MyTickets')" class="w-full text-left hover:bg-gray-800 p-2 rounded">My Ticket</button>
-            <button @click="navigateTo('/profile')" class="w-full text-left hover:bg-gray-800 p-2 rounded">Profile</button>
+            <button @click="navigateTo('/home')" class="w-full text-center hover:bg-gray-800 p-2 rounded">New Ticket</button>
+            <button @click="navigateTo('/MyTickets')" class="w-full text-center hover:bg-gray-800 p-2 rounded">My Ticket</button>
+            <button @click="navigateTo('/profile')" class="w-full text-center hover:bg-gray-800 p-2 rounded">Profile</button>
         </nav>
 
         <div class="mt-auto p-4">
